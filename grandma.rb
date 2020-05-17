38dee61ba3f87de5903d79deb212b15646121422
jassmine tatum <jassmine.tatum@gmail.com>
2020-05-17 14:42:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,8 +8,8 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
-d
-ef speak_to_grandma(phrase)
+
+def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
   else
